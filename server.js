e91aa21c19fe5b00bda6ec737b43235983d0efda
Alex Merced <alex@alexmerced.dev>
2023-01-08 16:00:11 -0500

    update

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express")
 const cors = require("cors")
 const PORT = process.env.PORT || 4000
 
+var whitelist = ['https://express-react-front-080123.onrender.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
 const app = express()
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 
 app.get("*", (req, res) => {
